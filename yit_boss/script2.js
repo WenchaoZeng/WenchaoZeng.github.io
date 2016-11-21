aaeaa1fdc1393e1f42438ed6d7b3bafcae8f9596
WenchaoZeng <zwc1986@gmail.com>
2016-11-21 15:08:53 +0800

    Update script2.js

--- yit_boss/script2.js
+++ yit_boss/script2.js
@@ -1,23 +1,13 @@
-function zwcReady() {
-    if (!window.jQuery) {
-        setTimeout(zwcReady, 100);
-        return;
-    }
-
-    window.jQuery.ready(function() {
-        
-        alert('ready');
-        
-        /* 去掉页头 */
-        setInterval(function() {
-          var ifr = document.getElementById('ifr');
-          if (ifr != null) {
-            ifr.height = window.innerHeight - 6;
-          }
-        }, 100);
-        
-        
-    });
-}
-
-setTimeout(zwcReady, 100);
+window.onload = function() {
+    
+    alert('Ready');
+    
+    /* 去掉页头 */
+    setInterval(function() {
+      var ifr = document.getElementById('ifr');
+      if (ifr != null) {
+        ifr.height = window.innerHeight - 6;
+      }
+    }, 100);
+    
+};
