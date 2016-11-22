1e7709d2daff52c444b23e71ca18443c07129edb
WenchaoZeng <zwc1986@gmail.com>
2016-11-22 11:24:42 +0800

    Update script2.js

--- yit_boss/script2.js
+++ yit_boss/script2.js
@@ -11,7 +11,7 @@ window.onload = function() {
     alert('test');
     /*
     
-    /* 调整内容高度 */
+    // 调整内容高度
     setInterval(function() {
       var ifr = document.getElementById('ifr');
       if (ifr != null) {
@@ -19,10 +19,10 @@ window.onload = function() {
       }
     }, 100);
     
-    /* 隐藏页头 */
+    // 隐藏页头
     $('body').append('<style>.navbar-fixed-top { display: none; } .sidebar, .hide-menu {top:0;} body {padding-top:0;}</style>');
     
-    /* 开启全部权限 */
+    // 开启全部权限
     $('#sidebar>div').attr('style', '');
     
     */
