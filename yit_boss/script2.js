e050032e652eabe938b14889b95b422a3022b613
WenchaoZeng <zwc1986@gmail.com>
2016-11-22 11:25:29 +0800

    Update script2.js

--- yit_boss/script2.js
+++ yit_boss/script2.js
@@ -9,7 +9,7 @@ window.onload = function() {
     }
     
     alert('test');
-    /*
+    
     
     // 调整内容高度
     setInterval(function() {
@@ -21,7 +21,7 @@ window.onload = function() {
     
     // 隐藏页头
     $('body').append('<style>.navbar-fixed-top { display: none; } .sidebar, .hide-menu {top:0;} body {padding-top:0;}</style>');
-    
+    /*
     // 开启全部权限
     $('#sidebar>div').attr('style', '');
     
