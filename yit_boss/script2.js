9f2106b5ad18ea71e96b93ea8e6daf87d17393b9
WenchaoZeng <zwc1986@gmail.com>
2016-11-22 11:43:00 +0800

    Update script2.js

--- yit_boss/script2.js
+++ yit_boss/script2.js
@@ -21,9 +21,9 @@ window.onload = function() {
     /*
     // 隐藏页头
     $('body').append('<style>.navbar-fixed-top { display: none; } .sidebar, .hide-menu {top:0;} body {padding-top:0;}</style>');
-    
+    */
     // 开启全部权限
     $('#sidebar>div').attr('style', '');
     
-    */
+    
 };
